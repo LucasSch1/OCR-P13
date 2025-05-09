aaf9f9b32b87d94540b05aacfd2f8e86f404a8c1
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-05-09 15:17:16 +0200

    Ajout de la mise a jour de la quantité dans la page du produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,15 +10,32 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProductController extends AbstractController
 {
     #[Route('/produit/{id}', name: 'app_view_detail_product', methods: ['GET'])]
-    public function getDetailProduct(ProduitRepository $produitRepository,int $id): Response
+    public function getDetailProduct(ProduitRepository $produitRepository, int $id, \App\Repository\PanierRepository $panierRepository, \App\Repository\PanierProduitsRepository $panierProduitsRepository): Response
     {
         $selectedProduct = $produitRepository->find($id);
         $imagePath = $this->getParameter('image_product_path');
+
+        $user = $this->getUser();
+        $quantite = 1;
+        $inCart = false;
+
+        if ($user) {
+            $panier = $panierRepository->findOneBy(['utilisateur' => $user, 'statut' => 'en_cours']);
+            if ($panier) {
+                $panierProduit = $panierProduitsRepository->findOneBy(['panier' => $panier, 'produit' => $selectedProduct]);
+                if ($panierProduit) {
+                    $quantite = $panierProduit->getQuantite();
+                    $inCart = true;
+                }
+            }
+        }
 
         return $this->render('product/detail_product.html.twig', [
             'controller_name' => 'ProductController',
             'product' => $selectedProduct,
             'imagePath' => $imagePath,
+            'quantite' => $quantite,
+            'inCart' => $inCart,
         ]);
     }
 }
