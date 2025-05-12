7751ac4c8e18042d28c021e79bfc3d333c2928b8
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-05-12 10:26:40 +0200

    Ajout de la fonction pour supprimer un utilisateur

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
 {
     #[Route('/utilisateur', name: 'app_user_account')]
-    public function showUserAccount(): Response
+    public function afficherCompteUtilisateur(): Response
     {
         $utilisateur = $this->getUser();
 
@@ -21,5 +23,25 @@ final class UserController extends AbstractController
             'controller_name' => 'UserController',
             'commandes' => $commandes,
         ]);
+    }
+
+    #[Route('/utilisateur/supprimer', name: 'app_user_delete_account')]
+    public function supprimerUtilisateur(Request $request,EntityManagerInterface $entityManager): Response
+    {
+        $utilisateur = $this->getUser();
+
+        if (!$utilisateur) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $entityManager->remove($utilisateur);
+        $entityManager->flush();
+
+        $this->container->get('security.token_storage')->setToken(null);
+        $request->getSession()->invalidate();
+
+        return $this->redirectToRoute('app_home');
+
+
     }
 }
