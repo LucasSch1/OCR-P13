d8dacd49c32412f51b173d968f71599b17e2c9a8
Lucas SCHNELZAUER <lucasschnelzauer@gmail.com>
2025-05-16 15:47:10 +0200

    Création d'un formulaire pour la quantité de produit

---
+++ src/Form/CartProductForm.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CartProductForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('quantity', IntegerType::class, [
+                'label' => 'Quantité :',
+                'attr' => [
+                    'min' => 1
+                ]
+            ]);
+    }
+}
